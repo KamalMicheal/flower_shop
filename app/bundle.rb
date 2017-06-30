966f9ca6031f2a08f2be882cdff330f80bd5c1c8
kamal <kamal.rizk@rea-group.com>
2017-07-01 07:28:47 +1000

    Sorting bundles by quantity

--- app/bundle.rb
+++ app/bundle.rb
@@ -2,7 +2,20 @@ class Bundle
   attr_reader :quantity, :price
 
   def initialize(quantity:, price:)
-    @quantity = quantity
-    @price = price
+    validate_params quantity, price
+    @quantity = Integer(quantity)
+    @price = Float(price)
+  end
+
+  private
+
+  def validate_params(quantity, price)
+    raise ArgumentError, "invalid bundle arguemnt" unless quantity.is_a?(Numeric) && price.is_a?(Numeric)
+  end
+
+  class << self
+    def sort(bundles)
+      bundles.sort_by(&:quantity)
+    end
   end
 end
--- spec/app/bundle_spec.rb
+++ spec/app/bundle_spec.rb
@@ -2,16 +2,54 @@ require 'spec_helper'
 
 describe Bundle do
   context '#new' do
-    before do
-      @bundle = Bundle.new(quantity: 5, price: 10)
+    context 'with valid parameters' do
+      before do
+        @bundle = Bundle.new(quantity: 5, price: 10)
+      end
+
+      it 'has a valid quantity' do
+        expect(@bundle.quantity).to eq 5
+      end
+
+      it 'has a valid price' do
+        expect(@bundle.price).to eq 10
+      end
     end
 
-    it 'has a valid quantity' do
-      expect(@bundle.quantity).to eq 5
+    context 'with invalid parameters' do
+      it 'raises an error for invalid quantity' do
+        expect { Bundle.new(quantity: 'abc', price: 10) }.to raise_error(ArgumentError)
+      end
+
+      it 'raises an error for invalid price' do
+        expect { Bundle.new(quantity: 15, price: '12.12.1') }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  context '#sort' do
+    let(:bundle1) { Bundle.new(quantity: 30, price: 1) }
+    let(:bundle2) { Bundle.new(quantity: 20, price: 15) }
+    let(:bundle3) { Bundle.new(quantity: 10, price: 110) }
+
+    let(:unsorted_bundles) do
+      [
+        bundle1,
+        bundle2,
+        bundle3
+      ]
     end
 
-    it 'has a valid price' do
-      expect(@bundle.price).to eq 10
+    let(:sorted_bundles) do
+      [
+        bundle3,
+        bundle2,
+        bundle1
+      ]
+    end
+
+    it 'sorts bundles by quantity asc' do
+      expect(Bundle.sort(unsorted_bundles)).to eq sorted_bundles
     end
   end
 end
